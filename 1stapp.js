bf3e0cdc937e30f01ffb237dc5d4056f05fd05f2
Paul <pmugisha@student.refactory.academy>
2024-03-06 13:09:35 +0300

    new changes

---
+++ 1stapp.js
@@ -0,0 +1,20 @@
+//numbers
+let numerOfBabies=100;
+//strings
+let sitterName="Alice";
+//undefined
+let babyAge;
+//null
+let numberOfBeds=0
+//boolean
+const working="true";
+let school={
+    numerOfBabies:100,
+    sitterName:"Alice",
+    work:teach()
+    function displayOnScreen(){
+let sum= 1+1
+document.getElementById("consoleAlt").innerHTML=sum;
+    }
+    
+}
